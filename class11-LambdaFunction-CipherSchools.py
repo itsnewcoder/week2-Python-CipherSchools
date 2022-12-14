60568ba8d19c3560e8f84786cb74cea46b89135e
Nikhil Raj <115399668+itsnewcoder@users.noreply.github.com>
2022-12-14 18:24:04 +0530

    Add files via upload

---
+++ class11-LambdaFunction-CipherSchools.py
@@ -0,0 +1,36 @@
+def func():
+    return 1+4
+    
+lambda a,b: a+b
+
+print("Hello World")
+
+show = print
+
+show("something")
+
+names=["Nikhil","Sanjeev","Raj","Sambhavi"]
+for name in enumerate(names):
+    print(name)
+
+
+a=5
+b=9
+
+a,b = b,a
+print(a,b)
+
+
+a=[1,2]
+b,c=a
+print(b,c)
+
+
+names=["Nikhil","Sanjeev","Raj","Sambhavi"]
+scores=[50,80,90,70]
+
+for name,score in zip(names,scores):
+    print(name,"-",score)
+
+
+
